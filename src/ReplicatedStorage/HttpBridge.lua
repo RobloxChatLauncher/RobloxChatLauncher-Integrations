28eac8aaff5bcc29861e046f7161abf1f4994771
Alina <wuangelina40@gmail.com>
2026-05-09 21:42:44 -0700

    infra: change all references to AlinaWan to RobloxChatLauncher for migration to org (#104)

--- src/ReplicatedStorage/HttpBridge.lua
+++ src/ReplicatedStorage/HttpBridge.lua
@@ -1,8 +1,8 @@
 --!language luau
 --[=[
-    Copyright (c) 2026 Riri <https://github.com/AlinaWan>
+    Copyright (c) 2026 Riri <https://github.com/RobloxChatLauncher>
 
-    Source: https://github.com/AlinaWan/RobloxChatLauncher/blob/main/integrations/
+    Source: https://github.com/RobloxChatLauncher/RobloxChatLauncher/blob/main/integrations/
     
     Licensed under the MPL 2.0 license.
     See https://www.mozilla.org/en-US/MPL/2.0/ for full text.
