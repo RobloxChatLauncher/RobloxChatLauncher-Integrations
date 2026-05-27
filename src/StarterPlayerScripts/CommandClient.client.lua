785e415bbe7b8b1a31133d98ec977c701f9f0b0f
AlinaWan <wuangelina40@gmail.com>
2026-05-26 23:52:35 -0700

    chore(integrations): update inline source links to point to the base repository

--- src/StarterPlayerScripts/CommandClient.client.lua
+++ src/StarterPlayerScripts/CommandClient.client.lua
@@ -2,7 +2,7 @@
 --[=[
     Copyright (c) 2026 Roblox Chat Launcher Developers & Contributors <https://github.com/RobloxChatLauncher>
 
-    Source: https://github.com/RobloxChatLauncher/RobloxChatLauncher-Integrations/blob/main/integrations/
+    Source: https://github.com/RobloxChatLauncher/RobloxChatLauncher-Integrations
     
     Licensed under the MPL 2.0 license.
     See https://www.mozilla.org/en-US/MPL/2.0/ for full text.
